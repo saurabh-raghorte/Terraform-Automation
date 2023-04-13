a51d0b9049b7b0293e8f25a8dfbbcbad8b660109
saurabh-raghorte <saurabhraghorte800@gmail.com>
2023-04-13 10:35:06 +0530

    3rd commit

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "keyname" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0f65bb365cb678b05" 
+    default     = "ami-0b9e15dd302531723" 
 }
 variable "versioning" {
     type        = bool
